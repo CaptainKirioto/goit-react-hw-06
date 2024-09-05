9b8601542d34af7930a4ef12e18c5015742c98e4
CaptainKirioto <mrg.zhi@gmail.com>
2024-09-05 17:55:30 +0200

    contactsSlice, partly filterSlice and store

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+  name: "filter",
+  initialState: {
+    value: "",
+  },
+  reducers: {
+    filter(state, action) {},
+  },
+});
